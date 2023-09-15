3143f553f9460b4dbbc6f2ccebd62d11327a7fef
Daniel Bertuzzi <30902283+dqbertuzzi@users.noreply.github.com>
2023-09-15 18:07:51 -0300

    Update flybondi-dash-monitor.py

--- flybondi-dash-monitor.py
+++ flybondi-dash-monitor.py
@@ -8,35 +8,56 @@ sheet_id = "18hHWaMBcvorBC9TRqBhG2HcGKpRZBdgZh3OqPw8ASus"
 dataFrame = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv", dtype={'Preco':float,
                                                                                                        'DataPesquisada':str,
                                                                                                        'IdaVolta':str})
+dff=dataFrame.to_dict('records')
 
 app = Dash(__name__)
 server = app.server
 
-# App layout
 app.layout = html.Div([
+    dcc.Store(id="storage", storage_type="memory", data=dff),
+    #dcc.Interval(id="timer", interval=1000*60, n_intervals=0),
     html.Div(children='Monitor de Preços Flybondi - São Paulo x Buenos Aires'),
+    html.Br(),
+    html.Button("Atualizar os dados",id='refresh-button', n_clicks=0),
     html.Hr(),
     html.Div(children='Escolha Data de Ida e Volta:'),
     html.Br(),
-    dcc.RadioItems(options=dataFrame['IdaVolta'].unique(), value=dataFrame['IdaVolta'].unique()[0], id='my-final-radio-item-example'),
+    dcc.Dropdown(options=['15/12/2023 - 21/12/2023', '16/12/2023 - 22/12/2023', '17/12/2023 - 23/12/2023'],
+                 value='15/12/2023 - 21/12/2023',
+                 id='my-final-radio-item-example'),
     dcc.Graph(figure={}, id='my-final-graph-example')
 ])
 
 # Add controls to build the interaction
 @callback(
-    Output(component_id='my-final-graph-example', component_property='figure'),
-    Input(component_id='my-final-radio-item-example', component_property='value')
+    Output(component_id='storage', component_property='data'),
+    Input(component_id='refresh-button', component_property='n_clicks'),
+    prevent_initial_call=True
 )
-
-def update_graph(col_chosen):
-    sheet_id = "18hHWaMBcvorBC9TRqBhG2HcGKpRZBdgZh3OqPw8ASus"
-    dataFrame = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv", dtype={'Preco':float,
+def refresh_data(n_clicks):
+    if n_clicks:
+        sheet_id = "18hHWaMBcvorBC9TRqBhG2HcGKpRZBdgZh3OqPw8ASus"
+        dataFrame = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv", dtype={'Preco':float,
                                                                                                        'DataPesquisada':str,
                                                                                                        'IdaVolta':str})
-    
-    
-    fig = px.bar(dataFrame[dataFrame['IdaVolta']==col_chosen], x='DataPesquisada', y='Preco')
+        
+    return dataFrame.to_dict('records')
+        
+@callback(
+    Output(component_id='my-final-graph-example', component_property='figure'),
+    Input(component_id='my-final-radio-item-example', component_property='value'),
+    Input("storage", "data"),
+    Input(component_id='refresh-button', component_property='n_clicks')
+)
+def update_graph(col_chosen, stored_dataframe, n_clicks):
+    dff = pd.DataFrame.from_records(stored_dataframe)
+        
+    fig = px.bar(dff[dff['IdaVolta']==col_chosen], x='DataPesquisada', y='Preco',
+                 labels={"Preco":"Preço (R$)", "DataPesquisada":"Data/Hora Pesquisada"}, text_auto=True,
+                 title="Histórico de Preços")
+    fig.update_traces(width=.2)
     return fig
+
 
 if __name__ == '__main__':
     app.run(debug=True)
