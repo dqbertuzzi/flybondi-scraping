9fb6f492db7e38cf5e38fcec803d475387b5d432
Daniel Bertuzzi <30902283+dqbertuzzi@users.noreply.github.com>
2023-09-15 12:35:40 -0300

    Add files via upload

---
+++ flybondi-dash-monitor.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+from datetime import datetime
+import pandas as pd
+from dash import Dash, html
+import plotly.express as px
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+
+sheet_id = "18hHWaMBcvorBC9TRqBhG2HcGKpRZBdgZh3OqPw8ASus"
+dataFrame = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv", dtype={'Preco':float,
+                                                                                                       'DataPesquisada':str,
+                                                                                                       'IdaVolta':str},
+                       parse_dates=['DataPesquisada'])
+
+app = Dash(__name__)
+
+# App layout
+app.layout = html.Div([
+    html.Div(children='Monitor de Preços Flybondi - São Paulo x Buenos Aires'),
+    html.Hr(),
+    html.Div(children='Escolha Data de Ida e Volta:'),
+    html.Br(),
+    dcc.RadioItems(options=dataFrame['IdaVolta'].unique(), value=dataFrame['IdaVolta'].unique()[0], id='my-final-radio-item-example'),
+    dcc.Graph(figure={}, id='my-final-graph-example')
+])
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id='my-final-graph-example', component_property='figure'),
+    Input(component_id='my-final-radio-item-example', component_property='value')
+)
+
+def update_graph(col_chosen):
+    fig = px.bar(dataFrame[dataFrame['IdaVolta']==col_chosen], x='DataPesquisada', y='Preco')
+    return fig
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
