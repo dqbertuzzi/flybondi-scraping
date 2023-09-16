d3f2a905ec27dc0048831ae00b3e6fa6025ae052
Daniel Bertuzzi <30902283+dqbertuzzi@users.noreply.github.com>
2023-09-16 11:38:04 -0300

    Update flybondi-dash-monitor.py

--- flybondi-dash-monitor.py
+++ flybondi-dash-monitor.py
@@ -10,7 +10,9 @@ dataFrame = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/expo
                                                                                                        'IdaVolta':str})
 dff=dataFrame.to_dict('records')
 
-app = Dash(__name__)
+app = Dash(__name__,
+          meta_tags=[{'name': 'viewport',
+                            'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}])
 server = app.server
 
 app.layout = html.Div([
@@ -50,21 +52,24 @@ def refresh_data(n_clicks):
     Input(component_id='refresh-button', component_property='n_clicks')
 )
 def update_graph(col_chosen, stored_dataframe, n_clicks):
+    def improve_text_position(x):
+        positions = ['top center', 'bottom center']  # you can add more: left center ...
+        return [positions[i % len(positions)] for i in range(len(x))]
+    
+   
     dff = pd.DataFrame.from_records(stored_dataframe)
     
     fig = px.line(dff[dff['IdaVolta']==col_chosen], x='DataPesquisada', y='Preco',
                   text="Preco", color_discrete_sequence =['#25291C'],
-                 labels={"Preco":"Preço (R$)", "DataPesquisada":"Data/Hora Pesquisada"}, title="Histórico de Preços",
-                 width=1400, height=400)
+                 labels={"Preco":"Preço (R$)", "DataPesquisada":"Data/Hora Pesquisada"}, title="Histórico de Preços")
     fig.update_traces(marker=dict(size=12),
                       line=dict(width=3),
-                      textposition="bottom left",
-                      textfont_size=14)
+                      textposition=improve_text_position(dff[dff['IdaVolta']==col_chosen]['Preco']),
+                      textfont_size=10)
     fig.update(layout_yaxis_range = [dff[dff['IdaVolta']==col_chosen]['Preco'].min()-10,dff[dff['IdaVolta']==col_chosen]['Preco'].max()+10])
     fig.update_xaxes(range=[-1, len(dff[dff['IdaVolta']==col_chosen]['DataPesquisada'].unique())])
-    
-    return fig
 
+    return fig
 
 if __name__ == '__main__':
     app.run(debug=True)
